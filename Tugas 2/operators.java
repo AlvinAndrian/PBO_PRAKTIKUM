bf21fbc142c64731edd6640929246d018fd9c0d1
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2021-09-23 19:50:45 +0700

    Soal No. 3

---
+++ Tugas 2/operators.java
@@ -0,0 +1,17 @@
+/* 
+Nama   : Alvin Andrian Rizki
+NIM    : 201511003
+Kelas  : D3 A2 JTK
+SOAL 3 OPERATORS
+*/
+
+import java.lang.Math;
+
+public class operators { // class operators
+    public static void main(String[] args) { // modul main
+        double x = 92.98; // variabel x bertipe double dengan nilai 92.98
+        int nx = (int) Math.round(x); // variabel nx bertipe integer dengan nilai konversi dari hasil Math.round variabel x
+        System.out.println(nx); // menampilkan nilai dari variabel nx
+    }
+    
+}
