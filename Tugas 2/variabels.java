f234a09701cc93a196cbb50cb3ad31dd0bced209
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2021-09-23 19:49:34 +0700

    Soal No. 2

---
+++ Tugas 2/variabels.java
@@ -0,0 +1,15 @@
+/* 
+Nama   : Alvin Andrian Rizki
+NIM    : 201511003
+Kelas  : D3 A2 JTK
+SOAL 2 VARIABELS
+*/
+   
+public class variabels { // class variabels
+  public static void main(Strings[] args) { // modul main
+    final double CM_PER_INCH = 2.54; // variabel konstanta
+    double paperWidth = 8.5; // variabel paperWirth bertipe double
+    double paperHeight = 11; // variabel paperHeight bertipe double
+    System.out.println("Paper size in centermeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH); // menampilkan hasilnya
+  }
+}
