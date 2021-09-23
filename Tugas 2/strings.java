b2de8b274c03c085ad231b33dc62e9884d4d6578
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2021-09-23 19:52:31 +0700

    Soal No. 5

---
+++ Tugas 2/strings.java
@@ -0,0 +1,17 @@
+/* 
+Nama   : Alvin Andrian Rizki
+NIM    : 201511003
+Kelas  : D3 A2 JTK
+SOAL 5 STRINGS
+*/
+
+public class strings { // class strings
+    public static void main(String[] args) { // modul main
+        // TODO code application logic here
+        String a = "Hello"; // variabel a bertipe string dengan nilai "Hello"
+        String b = "Java"; // variabel b bertipe string dengan nilai "Java"
+        System.out.println(a.length() + b.length()); // menampilkan jumlah panjang variabel a dan b
+        System.out.println("NO"); // Menampilkan tulisan "NO"
+        System.out.println(a + " " + b); // Menampilkan tulisan "Hello Java"
+    }
+}
