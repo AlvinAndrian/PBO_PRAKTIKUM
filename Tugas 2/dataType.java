bb2c8179e89e01d59d4a1b14aa9e3f79f5035972
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2021-09-25 09:00:00 +0700

    Update

--- Tugas 2/dataType.java
+++ Tugas 2/dataType.java
@@ -8,13 +8,13 @@ SOAL 1 TYPEDATA
 import java.util.Scanner;
 
 public class dataType{ // class dataType
-    public static void main(String []argh) { // modul main
+    public static void main(String []args) { // modul main
         Scanner input = new Scanner(System.in); // variabel input untuk menginput nilai dari keyboard
         for (int i=0;i<5;i++) { // Perulangan untuk 5 angka berulang
             try { // Supaya tidak terjadi error ketika dijalankan
                 long num=input.nextLong(); // variabel
-                System.out.println(x+" can be fitted in:"); // menampilkan tulisan "can be fitted in : "
-                if(num>=-128 && =num<=127) // penyeleksian kondisi jika num lebih dari sama dengan -128 dan jika kurang dari sama dengan 127
+                System.out.println(num+" can be fitted in:"); // menampilkan tulisan "can be fitted in : "
+                if(num>=-128 && num<=127) // penyeleksian kondisi jika num lebih dari sama dengan -128 dan jika kurang dari sama dengan 127
                     System.out.println("* byte"); // menampilkan tulisan "* byte"
                 if(num>=Short.MIN_VALUE && num<=Short.MAX_VALUE) // penyeleksian kondisi jika num lebih dari nilai minimum tipe data short dan jika kurang dari nilai maksimal tipe data short
                     System.out.println("* short"); // menampilkan tulisan "* short"
@@ -25,6 +25,7 @@ public class dataType{ // class dataType
             }
             catch(Exception e) { // supaya tidak terjadi error ketika dijalankan
                 System.out.println(input.next()+" can't be fitted anywhere."); // menampilkan nilai dari input.next() dan "can't be fitted anywhere."
+                input.close();
             }
         }
     }
